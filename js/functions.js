8086c1e8e56d9c18a271806d2fc372092721974c
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-12-07 13:00:15 +0300

    4

--- js/functions.js
+++ js/functions.js
@@ -24,20 +24,6 @@ $(() => {
 
 
 
-	// Скрол к пунктам меню
-	$(".scroll").on("click", function(e){
-		e.preventDefault();
-		let id = $(this).attr("href");
-
-		$("html, body").animate({
-				scrollTop: $(id).offset().top - 90
-			}, {
-				duration: 1500,
-				easing: "swing"
-		});
-	});
-
-
 	// Кнопка 'Вверх'
 	$('body').on('click', '.buttonUp button', function(e) {
 		e.preventDefault()
@@ -58,51 +44,15 @@ $(() => {
 	})
 
 
-
-	// Табы
-	var locationHash = window.location.hash
-
-	$('body').on('click', '.tabs button', function (e) {
-		e.preventDefault()
-
-		if (!$(this).hasClass('active')) {
-			const $parent = $(this).closest('.tabs_container'),
-				activeTab = $(this).data('content'),
-				$activeTabContent = $(activeTab),
-				level = $(this).data('level')
-
-			$parent.find('.tabs:first button').removeClass('active')
-			$parent.find('.tab_content.' + level).removeClass('active')
-
-			$(this).addClass('active')
-			$activeTabContent.addClass('active')
-		}
-	})
-
-	if (locationHash && $('.tabs_container').length) {
-		const $activeTab = $('.tabs button[data-content=' + locationHash + ']'),
-			$activeTabContent = $(locationHash),
-			$parent = $activeTab.closest('.tabs_container'),
-			level = $activeTab.data('level')
-
-		$parent.find('.tabs:first button').removeClass('active')
-		$parent.find('.tab_content.' + level).removeClass('active')
-
-		$activeTab.addClass('active')
-		$activeTabContent.addClass('active')
-
-		$('html, body').stop().animate({ scrollTop: $activeTabContent.offset().top }, 1000)
-	}
-
-	Fancybox
-	Fancybox.defaults.autoFocus = false
-	Fancybox.defaults.dragToClose = false
-	Fancybox.defaults.l10n = {
-		CLOSE: "Закрыть",
-		NEXT: "Следующий",
-		PREV: "Предыдущий",
-		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
-	}
+	// Fancybox
+	// Fancybox.defaults.autoFocus = false
+	// Fancybox.defaults.dragToClose = false
+	// Fancybox.defaults.l10n = {
+	// 	CLOSE: "Закрыть",
+	// 	NEXT: "Следующий",
+	// 	PREV: "Предыдущий",
+	// 	MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	// }
 
 
 	// Моб. версия
